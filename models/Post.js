437a2f1b24249753a5e5199cb955e9be1c4d6afe
Stephen Moore <judo2000@comcast.net>
2022-02-06 13:03:14 -0700

    Create Post model

---
+++ models/Post.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes, UUIDV4 } = require('sequelize');
+const sequelize = require('../config');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type:DataTypes.UUID,
+            defaultValues: UUIDV4,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.UUID,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        freezeTableName: true,
+        modelName: 'post'
+    }
+)
+
+module.exports = Post;
